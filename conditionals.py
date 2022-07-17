f0adb78f77ddfbf2e3de39cb91790873642686a8
Olakusibe Aremu-Oluwole <olashod@gmail.com>
2022-07-17 17:10:07 +0100

    addition of conditionals.py

---
+++ conditionals.py
@@ -0,0 +1,12 @@
+number = 1
+
+# the : notation denotes a block of code will follow/start
+
+if number == 1: 
+    anotherNumber = 2
+    anotherString = 'if i am a string'
+    print(anotherNumber)
+elif number == 2:
+    print("Number 2")
+else:
+    print(('Another number'))
