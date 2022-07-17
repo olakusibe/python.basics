889f9a588d5047b039ef5416be3093d9ffde8d12
Olakusibe Aremu-Oluwole <olashod@gmail.com>
2022-07-17 17:02:10 +0100

    add variables.py

---
+++ variables.py
@@ -0,0 +1,13 @@
+varibale_name_1 = "Hello World!" # string variable using double qoute
+varibale_name_2 = 'Hello World! again' # string variable using single qoute
+
+print(varibale_name_1)
+print(varibale_name_2)
+
+# there are other data types, whic are infered by their value 
+
+theNumber = 1
+theBoolean = True
+
+print(theNumber)
+print(theBoolean)
