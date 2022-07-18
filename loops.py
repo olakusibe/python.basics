eda1ebbe13e94b32027eb41bc36538ce3df156ec
Olakusibe Aremu-Oluwole <olashod@gmail.com>
2022-07-18 16:38:50 +0100

    addition of loops.py

---
+++ loops.py
@@ -0,0 +1,12 @@
+beatles = ["Paul", "John", "George", "Ringo"]
+
+# a for loop
+for member in beatles:
+    print(member)
+
+
+# a while loop
+count = 1
+while count <= 4:
+    print(beatles)
+    count = count + 1
